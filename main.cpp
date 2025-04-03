ef02d8ff51eb468cc6855dabe3dbe3022256c542
SurabhiKC <128141142+Surabhi-KC@users.noreply.github.com>
2025-04-04 03:45:14 +0530

    Implemented matrix_power

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ int main()
     
     int A[4] = {1, 2, 3, 4};
     int B[4] = {5, 6, 7, 8};
+    int n = 4;
     double* result = linalg.add(A, B, 2, 2);  // 2x2 matrix
     std::cout << "Matrix Addition Result:\n";
     for (int i = 0; i < 4; i++)
@@ -17,5 +18,21 @@ int main()
     }
 	double dt=linalg.dot(A,B,2,2);
 	std::cout<<"Dot product = "<<dt<<std::endl;
+
+    /*
+	    if (size <= 0) 
+    	    {
+        	std::cerr << ERROR: "Matrix size must be positive" << std::endl;
+        	return 1;
+            }
+    */
+    double* matpow = linalg.power(A, n);
+    std::cout << "Matrix Power Result:\n";
+    for (int i = 0; i < 4; i++)
+    {
+        std::cout << matpow[i] << " ";
+        if ((i + 1) % 2 == 0) std::cout << std::endl;
+    }
+
     return 0;
 }
