5bb47e9abe926e47848fd13aae09ef1c2bf24be5
SurabhiKC <128141142+Surabhi-KC@users.noreply.github.com>
2025-04-07 00:38:16 +0530

    Implemented qrdecomp

--- main.cpp
+++ main.cpp
@@ -1,136 +1,141 @@
 //This main file is just to test out the written functions. Ensure the library works as intended
 #include "linalg.h"
+#include <iomanip>
 
 int main()
 {
-    LinAlg linalg;
+	LinAlg linalg;
     
-    int A[2][2] = {{1, 2}, {3, 4}};
-    int B[2][2] = {{5, 6}, {7, 8}};
+    	int A[2][2] = {{1, 2}, {3, 4}};
+    	int B[2][2] = {{5, 6}, {7, 8}};
 	int C[4] = {1, 2, 3, 4};
-    int D[4] = {5, 6, 7, 8};
+    	int D[4] = {5, 6, 7, 8};
 	int E[2] = {3,7};
-    double (*result)[2] = linalg.add(A, B, 2);  // 2x2 matrix
-    std::cout << "Matrix Addition Result:\n";
-    for (int i = 0; i < 2; i++)
-    {
+
+    	double (*result)[2] = linalg.add(A, B, 2);  // 2x2 matrix
+    	std::cout << "Matrix Addition Result:\n";
+    	for (int i = 0; i < 2; i++)
+    	{
 		for (int j=0; j<2; j++)
 		{
 			std::cout << result[i][j] << " ";
 		}
 		std::cout << std::endl;
-    }
+    	}
+
 	double dt=linalg.dot(C,D,4);
 	std::cout<<"vector dot product"<<std::endl<<dt<<std::endl;
 	
 	double (*idt)[5]=linalg.identity<5>();
-	
 	std::cout << "Identity Matrix Result:\n";
-    for (int i = 0; i < 5; i++)
-    {
+    	for (int i = 0; i < 5; i++)
+    	{
 		for (int j=0; j<5; j++)
 		{
 			std::cout << idt[i][j] << " ";
 		}
 		std::cout << std::endl;
-    }
+    	}
 	
 	double (*matm)[2]= linalg.multiply(A,B,2,2);
 	std::cout << "Matrix Multiplication Result:\n";
-    for (int i = 0; i < 2; i++)
-    {
+    	for (int i = 0; i < 2; i++)
+    	{
 		for (int j=0; j<2; j++)
 		{
 			std::cout << matm[i][j] << " ";
 		}
 		std::cout << std::endl;
-    }
+    	}
 	
 	double (*cp)[2]= linalg.copy(A,2);
 	std::cout << "Matrix Copy Result:\n";
-    for (int i = 0; i < 2; i++)
-    {
+    	for (int i = 0; i < 2; i++)
+    	{
 		for (int j=0; j<2; j++)
 		{
 			std::cout << cp[i][j] << " ";
 		}
 		std::cout << std::endl;
-    }
+    	}
 	
 	double (*pows)[2]= linalg.power(A,5);
 	std::cout << "Matrix Power Result:\n";
-    for (int i = 0; i < 2; i++)
-    {
+    	for (int i = 0; i < 2; i++)
+    	{
 		for (int j=0; j<2; j++)
 		{
 			std::cout << pows[i][j] << " ";
 		}
 		std::cout << std::endl;
-    }
+    	}
 	
 	double (*otr)[4]= linalg.outer<4>(C, D);
 	std::cout << "Vector Outer Product Result:\n";
-    for (int i = 0; i < 4; i++)
-    {
+    	for (int i = 0; i < 4; i++)
+    	{
 		for (int j=0; j<4; j++)
 		{
 			std::cout << otr[i][j] << " ";
 		}
 		std::cout << std::endl;
-    }
+    	}
 	
 	double (*tp)[2]= linalg.transpose<2>(A);
 	std::cout << "Transpose of a Matrix Result:\n";
-    for (int i = 0; i < 2; i++)
-    {
+    	for (int i = 0; i < 2; i++)
+    	{
 		for (int j=0; j<2; j++)
 		{
 			std::cout << tp[i][j] << " ";
 		}
 		std::cout << std::endl;
-    }
+    	}
 	
 	int F[2][3] = {{1, 2, 3}, {4, 5, 6}};
+
 	double *vcm= linalg.vecmat(E,F,2);
 	std::cout << "Vector Matrix Product Result:\n";
-    for (int i = 0; i < 3; i++)
-    {
+    	for (int i = 0; i < 3; i++)
+    	{
 		std::cout << vcm[i] << " ";
-    }
+    	}
 	std::cout<<std::endl;
 	
 	int G[3]={1,2,3};
+
 	double *mtv= linalg.matvec(F,G,2);
 	std::cout << "Matrix Vector Product Result:\n";
-    for (int i = 0; i < 2; i++)
-    {
+    	for (int i = 0; i < 2; i++)
+    	{
 		std::cout << mtv[i] << " ";
-    }
+    	}
 	std::cout<<std::endl;
+	
 	double H[5][5] = {
-    {25, 15, -5, 10, 20},
-    {15, 18,  0,  8, 12},
-    {-5,  0, 11,  3,  2},
-    {10,  8,  3, 20,  5},
-    {20, 12,  2,  5, 30}};
+    		{25, 15, -5, 10, 20},
+    		{15, 18,  0,  8, 12},
+	    	{-5,  0, 11,  3,  2},
+    		{10,  8,  3, 20,  5},
+    		{20, 12,  2,  5, 30}};
 	
 	double I[3][3]= {{4,12,-16},{12,37,-43},{-16,-43,98}};
 	
 	double (*chl)[5]=linalg.cholesky(H);
 	std::cout << "Cholesky decomposition of a Matrix Result:\n";
-    for (int i = 0; i <5; i++)
-    {
+    	for (int i = 0; i <5; i++)
+    	{
 		for (int j=0; j<5; j++)
 		{
 			std::cout << chl[i][j] << " ";
 		}
 		std::cout << std::endl;
-    }
+    	}
 	
 	double dtm=linalg.determinant(I,3);
 	std::cout<<"Determinant of the Matrix"<<std::endl<<dtm<<std::endl;
 
-	double (*inv)[3]=linalg.inverse(I, 3);
+	double (*inv)[3] = linalg.inverse(I, 3);
 	if(inv)
 	{
 		std::cout << "Inverse of a Matrix Result:\n";
@@ -142,10 +147,31 @@ int main()
 			}
         		std::cout << "\n";
     		}
+	}
     	else 
 	{
         	std::cout << "\nMatrix is singular or not invertible.\n";
     	}
 
-    return 0;
+	auto qrdec = linalg.qrdecomp(I, 3);
+    	double* Q = qrdec.first;
+    	double* R = qrdec.second;
+	std::cout << "QR Decompostion of a Matrix:\n";
+	std::cout << "Matrix Q:\n";
+    	for (int i = 0; i < 3; ++i) 
+	{
+        	for (int j = 0; j < 3; ++j)
+            		std::cout << std::setw(10) << std::fixed << std::setprecision(5) << Q[i * 3 + j] << " ";
+       		std::cout << "\n";
+    	}
+
+    	std::cout << "\nMatrix R:\n";
+    	for (int i = 0; i < 3; ++i) 
+	{
+	        for (int j = 0; j < 3; ++j)
+            		std::cout << std::setw(10) << std::fixed << std::setprecision(5) << R[i * 3 + j] << " ";
+        	std::cout << "\n";
+    	}
+
+    	return 0;
 }
